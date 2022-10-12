9afc27d49a3c5dbcdc2b146040e28fdfe4b13248
U-BSD\411915 <411915@SHS-HJ86ND2.beaverton.k12.or.us>
2022-10-12 14:16:17 -0700

    a'

---
+++ studentlist.cpp
@@ -0,0 +1,23 @@
+//Kevin Yu
+//11-4-22
+
+//studnet s stores input from user
+//then utrasnfer it to a vector
+//and u can reusre s
+
+#include <iostream>
+
+using namespace std;
+
+struct Student {
+	char fname[30];
+	char lname[30];	
+	int id;
+	float gpa;
+};
+
+int main()
+{
+
+	Student s;
+}
