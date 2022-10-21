477b7106c0c25ef064878fa3924452929d324ed4
U-BSD\411915 <411915@SHS-HJ86ND2.beaverton.k12.or.us>
2022-10-21 14:24:21 -0700

    just do delete

--- studentlist.cpp
+++ studentlist.cpp
@@ -1,12 +1,10 @@
 //Kevin Yu
 //11-4-22
 
-//studnet s stores input from user
-//then utrasnfer it to a vector
-//and u can reusre s
-
+#include <vector>
 #include <iostream>
 #include <cstring>
+#include <iomanip>
 
 using namespace std;
 
@@ -17,9 +15,64 @@ struct Student {
 	float gpa;
 };
 
+void add(vector<Student*> &storage);
+void del(vector<Student*> &storage);
+void print(vector<Student*> &storage);
+
 int main() {
+	bool play = true;
+
+	vector<Student*> storage;
+
+	while (play == true) {
+		
+		//Gets add or delete 
+		char str[10]; 
+		Student s;
+		cout << "Input: ADD, DELETE, PRINT, or QUIT: "; 
+		cin.get(str, 10);
+		cin.get();
+
+		//Checks if add or delete
+		if (strcmp(str, "ADD") == 0) {
+			add(storage);
+		}
+		else if (strcmp(str, "DELETE") == 0) {
+			cout << "delete";
+		}
+		else if (strcmp(str, "PRINT") == 0) {	
+			print(storage);
+		}
+		else if (strcmp(str, "QUIT") == 0) {
+			play = false;	
+		}
+		else {
+			cout << "Invalid Input" << endl;
+		}
+	}
+}
+
+void add(vector<Student*> &storage) {
+	Student* s = new Student();
+	cout << "First name: ";
+	cin >> s->fname;
+	cout << "Last name: ";
+	cin >> s->lname;
+	cout << "ID: ";
+	cin >> s->id;
+	cout << "GPA: ";
+	cin >> s->gpa;
+	cin.ignore(10, '\n');
+	storage.push_back(s);
+}
+
+void del(vector<Student*> &storage) {
+	cout << "ID: ";
 	
-	Student s;
-	cout << "Add or Delete? ";
-	
+}
+
+void print(vector<Student*> &storage) {	
+	for (vector<Student*>::iterator it = storage.begin(); it != storage.end(); ++it) {
+		cout << (*it)->fname << ' ' << (*it)->lname << ", " << (*it)->id << ", " << fixed << setprecision(2) << (*it)->gpa << endl;
+	}
 }
