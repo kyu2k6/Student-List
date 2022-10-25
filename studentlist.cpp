2b3a0e0272f67eb1f5063ff7acab782e2acf15f8
U-BSD\411915 <411915@SHS-HJ86ND2.beaverton.k12.or.us>
2022-10-25 13:54:30 -0700

    Final Version

--- studentlist.cpp
+++ studentlist.cpp
@@ -1,6 +1,7 @@
 //Kevin Yu
 //11-4-22
 
+#include <algorithm>
 #include <vector>
 #include <iostream>
 #include <cstring>
@@ -8,6 +9,7 @@
 
 using namespace std;
 
+//student struct for all the students
 struct Student {
 	char fname[30];
 	char lname[30];	
@@ -22,28 +24,29 @@ void print(vector<Student*> &storage);
 int main() {
 	bool play = true;
 
+	//storage for all the student structs	
 	vector<Student*> storage;
 
 	while (play == true) {
 		
-		//Gets add or delete 
+		//Gets add or delete or print or quit 
 		char str[10]; 
 		Student s;
 		cout << "Input: ADD, DELETE, PRINT, or QUIT: "; 
 		cin.get(str, 10);
 		cin.get();
 
-		//Checks if add or delete
+		//Checks if add or delete or print or quit
 		if (strcmp(str, "ADD") == 0) {
 			add(storage);
 		}
-		else if (strcmp(str, "DELETE") == 0) {
-			cout << "delete";
+		if (strcmp(str, "DELETE") == 0) {
+			del(storage);
 		}
-		else if (strcmp(str, "PRINT") == 0) {	
+		if (strcmp(str, "PRINT") == 0) {	
 			print(storage);
 		}
-		else if (strcmp(str, "QUIT") == 0) {
+		if (strcmp(str, "QUIT") == 0) {
 			play = false;	
 		}
 		else {
@@ -53,6 +56,7 @@ int main() {
 }
 
 void add(vector<Student*> &storage) {
+	//makes new student
 	Student* s = new Student();
 	cout << "First name: ";
 	cin >> s->fname;
@@ -63,16 +67,29 @@ void add(vector<Student*> &storage) {
 	cout << "GPA: ";
 	cin >> s->gpa;
 	cin.ignore(10, '\n');
+	//pushues new studnet into vector
 	storage.push_back(s);
 }
 
 void del(vector<Student*> &storage) {
+	int tmpID;
 	cout << "ID: ";
-	
-}
-
-void print(vector<Student*> &storage) {	
+	cin >> tmpID;
+	cin.ignore(10, '\n');
+	//goes through each element in the vector and checks if the id value is equal to the inputed one
 	for (vector<Student*>::iterator it = storage.begin(); it != storage.end(); ++it) {
+		if ((*it)->id == tmpID) {
+			//delete from memory
+			delete *it;
+			//delete from storage
+			storage.erase(it);	
+			break;
+		}
+	}
+}
+void print(vector<Student*> &storage) {	
+	//goes through each element in the vector and prints out all the structs
+	for (vector<Student*>::iterator it = storage.begin(); it != storage.end(); ++it) {          //cut off to 2 decimal
 		cout << (*it)->fname << ' ' << (*it)->lname << ", " << (*it)->id << ", " << fixed << setprecision(2) << (*it)->gpa << endl;
 	}
 }
